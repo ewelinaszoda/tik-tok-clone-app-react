db219d5d681968fee003df3c4f954253ffd27429
ewelinaszoda <ewelinaszoda@gmail.com>
2020-09-14 23:40:20 +0100

    set firebase

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyARB9BdFtm7_ouues9d0WCDxwlv-Ql5bqk",
+  authDomain: "tic-tok-react.firebaseapp.com",
+  databaseURL: "https://tic-tok-react.firebaseio.com",
+  projectId: "tic-tok-react",
+  storageBucket: "tic-tok-react.appspot.com",
+  messagingSenderId: "906877756971",
+  appId: "1:906877756971:web:9d1376e82eeb68cd282c6e",
+  measurementId: "G-F5D0412C8F"
+};
