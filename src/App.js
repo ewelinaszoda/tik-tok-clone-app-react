fe64da3750f2a907792af25a0457737c0a3a1e71
ewelinaszoda <ewelinaszoda@gmail.com>
2020-09-16 23:55:33 +0100

    create video component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     // BEM
     <div className="app">
       <h1>hello! Let's built Tik Tok Clone</h1>
+      <div className="app__videos">
+        <Video />
+        <Video />
+        <Video />
+        <Video />
+      </div>
     </div>
   );
 }
