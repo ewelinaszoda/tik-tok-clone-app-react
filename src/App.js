2cb863f5b3d685f93a5838a4c2b91fe94fac5b7c
ewelinaszoda <ewelinaszoda@gmail.com>
2020-09-17 22:49:12 +0100

    done snap functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Video from "./Video"
 import './App.css';
 
 function App() {
---
+++ src/Video.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Video.css'
+
+const Video = () => {
+  return (
+    <div className="video">
+      <h1>This is a video</h1>
+      {/* <VideoFooter />
+      <VideoSidebar /> */}
+    </div>
+  )
+}
+
+export default Video
