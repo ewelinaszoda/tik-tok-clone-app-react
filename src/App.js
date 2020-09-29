35f7ead52625ff17474ce43e4754ba6e8b80ec5e
ewelinaszoda <ewelinaszoda@gmail.com>
2020-09-29 23:19:16 +0100

    Com
    '
    exit

--- src/App.js
+++ src/App.js
@@ -8,12 +8,20 @@ function App() {
     <div className="app">
       {/* center video only horizontal */}
       {/* <center> */}
-        <div className="app__videos">
-          <Video />
-          <Video />
-          <Video />
-          <Video />
-        </div>
+      <div className="app__videos">
+        <Video
+          url=""
+          channel="evie"
+          description="WOW is working"
+          song="The happy song"
+          likes={123}
+          message={400}
+          shares={200}
+        />
+        <Video />
+        <Video />
+        <Video />
+      </div>
       {/* </center> */}
     </div>
   );
--- src/Video.js
+++ src/Video.js
@@ -4,8 +4,8 @@ import VideoSidebar from './VideoSidebar';
 import './Video.css';
 
 
-const Video = () => {
-  const [playing, setPlaying] = useState(false)
+const Video = ({url, channel, description, song, likes, messages, shares}) => {
+  const [playing, setPlaying] = useState(false) 
   const videoRef = useRef(null);
 
   const onVideoPress = () => {
@@ -25,15 +25,17 @@ const Video = () => {
         loop
         onClick={onVideoPress}
         ref={videoRef}
-        src="https://v19-web.tiktokcdn.com/b804d2b5adbbaeb1461567af91d5cd66/5f667716/video/tos/useast2a/tos-useast2a-ve-0068c002/721b97b3b943478b914e20d0b3a9b056/?a=1988&br=2822&bt=1411&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=202009191524270101902192185F079296&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=M3A1a2kzZXA8djMzNjczM0ApZTg5NDU5ZWRlNzg5ZTs8OGdoZXIwcWVlc2JfLS01MTZzcy4vMWMxNjRgYjIwMzJhNjA6Yw%3D%3D&vl=&vr="
+        src={url}
       ></video>
       <VideoFooter 
-      channel="evie" 
-      description="tWe are the champions" 
-      song="Usher - Yeah"/>
+      channel={channel}
+      description={description}
+      song={song}/>
       <VideoSidebar 
       likes={likes} 
-      messages={messages}/>
+      messages={messages}
+      shares={shares}
+      />
     </div>
   )
 }
